c95898106fc29066d1e8f2458e3f8edeccfaa79c
stiks84@gmail.com <Rfhfv,jkm14>
2020-06-13 15:08:18 +0500

    "class and object - cat experiments"

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,30 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat murzik = new Cat();
+        Cat barsik = new Cat();
 
-        System.out.println(cat.getStatus());
+        System.out.println(murzik.getStatus());
+        System.out.println(barsik.getStatus());
+        System.out.println("*********************");
+        System.out.println(murzik.getWeight());
+        System.out.println(barsik.getWeight());
+        System.out.println("*********************");
+        murzik.feed(100.0);
+        barsik.meow();
+        System.out.println(murzik.getWeight());
+        System.out.println(barsik.getWeight());
+        System.out.println("*********************");
+        while (murzik.getWeight()<9000.0){
+            murzik.feed(100.0);
+            murzik.drink(50.0);
+        }
+        while (barsik.getWeight()>1000.0){
+            barsik.meow();
+        }
+        System.out.println(murzik.getStatus());
+        System.out.println(barsik.getStatus());
+        System.out.println("*********************");
+
     }
 }
