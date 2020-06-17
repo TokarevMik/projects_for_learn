d1ab81c7d75f2c572fc97bdcca0a98360baa4634
stiks84@gmail.com <Rfhfv,jkm14>
2020-06-17 16:53:27 +0500

    'les 3 - static methods'

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,4 +1,3 @@
-
 public class Cat {
     private double originWeight;
     private double weight;
@@ -6,36 +5,66 @@ public class Cat {
     private double minWeight;
     private double maxWeight;
     private double countOfFood = 0.0;
+    private static int catCount = 0;
+    private boolean alive;
 
     public Cat() {
+        catCount++;
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        alive = true;
 
+    }
+    public boolean isAlive(double weight){
+        if(weight < minWeight|weight>maxWeight){
+            alive = false;
+        }
+        return alive;
+    }
+    public static int getCatCount() {
+
+        return catCount;
     }
 
     public void meow() {
-        weight = weight - 1;
-        System.out.println("Meow");
+        if( isAlive(weight)){
+            weight = weight - 1;
+            System.out.println("Meow");
+        }else {
+            System.out.println("Can't meow. Cai is dead");
+        }
     }
     public void pee(){
-        weight = weight - 3.0;
-        System.out.println("Cat lost 3 g. of weight");
+        if( isAlive(weight)){
+            weight = weight - 3.0;
+            System.out.println("Cat lost 3 g. of weight");
+        } else {
+            System.out.println("Can't meow. Cat is dead");
+        }
     }
-    public void feed(Double amount) {
-        weight = weight + amount;
-        countOfFood = countOfFood + amount;
+    public void feed(double amount) {
+        if( isAlive(weight)){
+            weight = weight + amount;
+            countOfFood = countOfFood + amount;
+        }else {
+            System.out.println("Can't eat. Cat is dead");
+        }
     }
 
-    public void drink(Double amount) {
-        weight = weight + amount;
-        countOfFood = countOfFood + amount;
+    public void drink(double amount) {
+        if( isAlive(weight)) {
+            weight = weight + amount;
+            countOfFood = countOfFood + amount;
+        } else{
+            System.out.println("Can't drink. Cat is dead");
+        }
     }
 
-    public double getCountOfFood() {
-        return countOfFood;
-    }
+    //public double getCountOfFood() {
+    //    return countOfFood;
+    //}
 
     public Double getWeight() {
         return weight;
@@ -43,8 +72,10 @@ public class Cat {
 
     public String getStatus() {
         if (weight < minWeight) {
+            catCount--;
             return "Dead";
         } else if (weight > maxWeight) {
+            catCount--;
             return "Exploded";
         } else if (weight > originWeight) {
             return "Sleeping";
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -4,15 +4,16 @@ public class Loader
     public static void main(String[] args)
     {
         Cat murzik = new Cat();
-        System.out.println("Кот весит " + murzik.getWeight());
-        murzik.feed(150.0);
-        murzik.pee();
-        murzik.pee();
-        murzik.pee();
-        murzik.pee();
-        System.out.println("******************");
-        System.out.println("Теперь кот весит "+murzik.getWeight());
-        System.out.println("Кот съел " + murzik.getCountOfFood());
+        Cat musya = new Cat();
+        System.out.println("Murzik  is " + murzik.getStatus());
+        System.out.println("Musya  is " + musya.getStatus());
+        System.out.println("Murzik's weight is " + murzik.getWeight());
+        while (murzik.isAlive(murzik.getWeight())){
+            murzik.feed(100.0);
+        }
+        murzik.feed(200.0);
+        System.out.println("Murzik is " + murzik.getStatus());
+
 
 
 
