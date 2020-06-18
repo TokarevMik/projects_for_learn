48fd29519448982d1a264a82291418770093a076
stiks84@gmail.com <Rfhfv,jkm14>
2020-06-19 00:05:06 +0500

    les 8 'out libs' upd

--- 03_ObjectsAndClasses/Airport/src/Main.java
+++ 03_ObjectsAndClasses/Airport/src/Main.java
@@ -1,9 +1,14 @@
 import com.skillbox.airport.Airport;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Airport airport = Airport.getInstance();
         System.out.println(airport.getAllAircrafts());
+        System.out.println("*************************");
+        List aircraft = airport.getAllAircrafts();
+        System.out.println(aircraft.size());
         
     }
 }
