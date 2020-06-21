d5c56814db16f1d8ea4b5b47b79f8a277fbcaabb
stiks84@gmail.com <Rfhfv,jkm14>
2020-06-21 15:30:37 +0500

    homeWork 4.1 ' IntegerExperiments'

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -4,12 +4,25 @@ public class Main
     {
         Container container = new Container();
         container.count += 7843;
-
+        Integer a = 12345;
+        Integer b = 10;
+        Integer c = 5059191;
+        System.out.println(sumDigits(a));
+        System.out.println(sumDigits(b));
+        System.out.println(sumDigits(c));
     }
 
-    public Integer sumDigits(Integer number)
+    public static Integer sumDigits(Integer number)
     {
+        Integer sum = 0;
+        String num = number.toString();
+        int count  = num.length();
+        for (int i = 0; i <count ; i++) {
+            char a = num.charAt(i);
+            String b = String.valueOf(a);
+            sum += Integer.parseInt(b);
+           }
         //@TODO: write code here
-        return 0;
+        return sum;
     }
 }
