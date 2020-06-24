20827fc79820a5d512bbbb638352924138ac1f8f
stiks84@gmail.com <Rfhfv,jkm14>
2020-06-24 23:36:03 +0500

    4.5 'StringExperiments'***

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -4,15 +4,16 @@ public class Loader
     public static void main(String[] args)
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        int v1 = text.indexOf('5');
-        int v2 = text.indexOf('у') - 2;
-        String cash1 =text.substring(v1,v2);
-        int p1 = text.indexOf('-') + 1;
-        int p2 = text.indexOf('3') + 1;
-        String cash2 =(text.substring(p1,p2)).trim();
-        int m1 = text.lastIndexOf('3');
-        int m2 = text.lastIndexOf('р');
-        String cash3 =(text.substring(m1,m2)).trim();
-        System.out.println(Integer.parseInt(cash1)+Integer.parseInt(cash2) + Integer.parseInt(cash3));
+        String text2 = text.replaceAll("[^ 0-9]","");
+        System.out.println(text2);
+        String [] nums = text2.split("\\s+");
+        int sum = 0;
+        for (String a: nums) {
+            System.out.println("*******");
+            System.out.println(a);
+            //System.out.println(Integer.parseInt(a.trim()));
+        }
+        //System.out.println(sum);
+
     }
 }
