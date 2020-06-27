c28682ac124b8afa832e9bc116964e6ec97eb7e3
stiks84@gmail.com <Rfhfv,jkm14>
2020-06-27 16:16:21 +0500

    4.5 "task 3 - Name Surname Patron regex"

--- 04_NumbersStringsAndDates/Name/src/Main.java
+++ 04_NumbersStringsAndDates/Name/src/Main.java
@@ -7,31 +7,15 @@ public class Main {
         String surname = null;
         String name = null;
         String patron = null;
-        int spaceIndex = -1;
-        int countInd = 0;//счетчик пробелов
-       while(true){
-           spaceIndex = fio.indexOf(" ",spaceIndex+1);
-           if(spaceIndex == -1){
-               break;
-           }
-            countInd++;
-       }
-       if(countInd==1){                                 //введено 2 слова
-           surname = fio.substring(0,fio.indexOf(" "));
-           name = fio.substring(surname.length());
-           patron = "без отчества";
-       } else if(countInd==2){                          //введено 3 слова
-           surname = fio.substring(0,fio.indexOf(" "));
-           int indexSur = fio.indexOf(" ",surname.length()+1);
-           name = fio.substring(surname.length(),indexSur);
-           patron = fio.substring(name.length()+surname.length());
-       } else if (countInd>2){                         //введено более 3 слов
-           surname = fio.substring(0,fio.indexOf(" "));
-           int indexSur = fio.indexOf(" ",surname.length()+1);
-           name = fio.substring(surname.length(),indexSur);
-           int indexPatron = fio.indexOf(" ",name.length()+surname.length()+1);
-           patron = fio.substring(name.length()+surname.length(),indexPatron);
-       }
+        //int spaceIndex = -1;
+        String [] fio2 = fio.split("\\s");
+        surname = fio2[0];
+        name  = fio2[1];
+        if(fio2.length>2){
+            patron = fio2[2];
+        } else {
+            patron = "без отчества";
+        }
         System.out.println("Фамилия: " + surname);
         System.out.println("Имя: " + name.trim());
         System.out.println("Отчество: " + patron.trim() );
