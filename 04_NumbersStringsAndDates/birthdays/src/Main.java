987048cf12f24e2b6d2ca499347f3be35c64fc06
stiks84@gmail.com <Rfhfv,jkm14>
2020-06-29 22:16:19 +0500

    4.6 'Birth Day upd.'

--- 04_NumbersStringsAndDates/birthdays/src/Main.java
+++ 04_NumbersStringsAndDates/birthdays/src/Main.java
@@ -6,16 +6,19 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
         int day = 14;
-        int month = 02;
+        int month = 10;
         int year = 1984;
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd - E", Locale.ENGLISH);
-        Calendar calendar = Calendar.getInstance();
-        for (int i = 0; i <36 ; i++) {
+        Calendar birthDay = Calendar.getInstance();
+        birthDay.set(year,month,day);
+        Calendar today = Calendar.getInstance();
+        int counter = 0;
+        while(today.after(birthDay)) {
+           System.out.println(counter + " " + simpleDateFormat.format(birthDay.getTime()));
+            birthDay.set(year,month,day);
             year++;
-            calendar.set(year,month,day);
-            System.out.println(i + " " + simpleDateFormat.format(calendar.getTime()));
+            counter++;
         }
-
 
     }
 }
