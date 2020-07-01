963b15ecc12db69073053ab122b8b3b28a0c99c9
stiks84@gmail.com <Rfhfv,jkm14>
2020-07-02 00:26:20 +0500

    5.1 task 2 "temperature" upd1

--- 05_ArraysAndCollections/Temperature/src/Main.java
+++ 05_ArraysAndCollections/Temperature/src/Main.java
@@ -4,26 +4,28 @@ public class Main {
     public static final float MAX_TEMPERATURE = 40.0f;
     public static final float MIN_HEALTH = 36.2f;
     public static final float MAX_HEALTH = 36.9f;
+
     public static void main(String[] args) {
 
-        final int patients  = 10;
+        final int patients = 10;
         float temperatureOfAll = 0;
-        float [] temperature = new float[patients];
+        float[] temperature = new float[patients];
         System.out.print("Тeмпературы всех пациентов: ");
-        for (int i = 0; i <patients ; i++) {
-            temperature[i] = MIN_TEMPERATURE + (float) (Math.random()*(MAX_TEMPERATURE-MIN_TEMPERATURE));
+        for (int i = 0; i < patients; i++) {
+            temperature[i] = MIN_TEMPERATURE + (float) (Math.random() * (MAX_TEMPERATURE - MIN_TEMPERATURE));
             System.out.print(temperature[i] + " ");
             isHealth(temperature[i]);
-            temperatureOfAll+=temperature[i];
+            temperatureOfAll += temperature[i];
         }
         System.out.println();
-        System.out.println("Средняя температура по больнице " + (temperatureOfAll/patients));
+        System.out.println("Средняя температура по больнице " + (temperatureOfAll / patients));
         System.out.println("Количество здоровых: " + healthCount);
 
     }
-    static void isHealth(float t){
-        if(t>=MIN_HEALTH&&t<=MAX_HEALTH){
-           healthCount++;
+
+    static void isHealth(float t) {
+        if (t >= MIN_HEALTH && t <= MAX_HEALTH) {
+            healthCount++;
         }
     }
 }
