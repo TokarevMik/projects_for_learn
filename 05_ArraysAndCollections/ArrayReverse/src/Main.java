c3d79518eb6dc26b66eeb6e898af0dc139d3ae9b
stiks84@gmail.com <Rfhfv,jkm14>
2020-07-02 00:17:44 +0500

    5.1 task 1 "Array reverse" upd

--- 05_ArraysAndCollections/ArrayReverse/src/Main.java
+++ 05_ArraysAndCollections/ArrayReverse/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String [] rainbow = {"Каждый", "хотник", "желает", "знать", "где", "сидит", "фазан"};
-        for (int i = 0; i <4 ; i++) {
+        String [] rainbow = {"Каждый", "охотник", "желает", "знать", "где", "сидит", "фазан"};
+        for (int i = 0; i <=rainbow.length/2 ; i++) {
             String a = rainbow[i];
             rainbow[i] = rainbow[rainbow.length-1-i];
             rainbow[rainbow.length-1-i] = a;
