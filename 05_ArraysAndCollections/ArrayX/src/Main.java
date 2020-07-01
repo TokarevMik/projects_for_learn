0d17768d992bd074d89cd70e5eec13083eb3854f
stiks84@gmail.com <Rfhfv,jkm14>
2020-07-02 00:03:54 +0500

    5.1 task * "X" upd

--- 05_ArraysAndCollections/ArrayX/src/Main.java
+++ 05_ArraysAndCollections/ArrayX/src/Main.java
@@ -1,23 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         String[][] X = new String[7][7];
-        for (int i = 0; i <7; i++) {
-             X[i][i] = "*";
-             X[i][6-i] ="*";
-            for (int j = 0; j <7 ; j++) {
-                if(j!=i&&j!=(6-i)){
-                    X[i][j]=" ";
+        for (int i = 0; i < X.length; i++) {
+            X[i][i] = "*";
+            X[i][X.length - 1 - i] = "*";
+            for (int j = 0; j < X.length; j++) {
+                if (j != i && j != (X.length - 1 - i)) {
+                    X[i][j] = " ";
                 }
-
             }
         }
-        for (int i = 0; i <X.length ; i++) {
-
-            for (int j = 0; j <X[i].length ; j++) {
+        for (int i = 0; i < X.length; i++) {
+            for (int j = 0; j < X[i].length; j++) {
                 System.out.print(X[i][j]);
             }
             System.out.println();
-
         }
     }
 }
