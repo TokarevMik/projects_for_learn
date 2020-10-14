bba86730c4fa4cef09dc94e62a0b105fd96d62ad
stiks84@gmail.com <Rfhfv,jkm14>
2020-10-14 15:57:28 +0500

    9.2.2 'CopyAndPaste' 1

--- 09_FilesAndNetwork/CopyAndPaste/src/Main.java
+++ 09_FilesAndNetwork/CopyAndPaste/src/Main.java
@@ -5,40 +5,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //for (; ; ) {
+        for (; ; ) {
             try {
                 System.out.println("Введите путь копируемой папки ");
                 Scanner scn = new Scanner(System.in);
                 String firstDir = scn.nextLine();
                 Path path1 = Paths.get(firstDir);
-//                if (!Files.isDirectory(path1) && !Files.exists(path1)) {
-//                    System.out.println("Не верный адрес");
-//                } else {
-//                    for (; ; ) {
+                if (!Files.isDirectory(path1) && !Files.exists(path1)) {
+                    System.out.println("Не верный адрес");
+                } else {
+                    for (; ; ) {
                         System.out.println("Введите путь папки назначения ");
                         scn = new Scanner(System.in);
                         Path path2 = Paths.get(scn.nextLine());
-//                        if (!Files.isDirectory(path2) && !Files.exists(path2)) {
-//                            System.out.println("Не верный адрес");
-//                        } else {
+                        if (!Files.isDirectory(path2) && !Files.exists(path2)) {
+                            System.out.println("Не верный адрес");
+                        } else {
                             Files.walkFileTree(path1, new MyFileVisitor(path1, path2));
-//                        }
-//                    }
-//                }
-            }
-            catch (AccessDeniedException e){
-                System.out.println("Нет доступа 1"  );
+                            break;
+                        }
+                    }
+                }
+            } catch (AccessDeniedException e) {
+                System.out.println("Нет доступа 1");
                 e.printStackTrace();
-            }
-            catch (Exception ex) {
+            } catch (Exception ex) {
                 System.out.println("Нет доступа 2");
                 ex.printStackTrace();
             }
-       // }
+        }
     }
 }
 
-class MyFileVisitor implements FileVisitor<Path> {
+class MyFileVisitor extends SimpleFileVisitor<Path> {
     Path src;
     Path dest;
 
@@ -51,9 +50,8 @@ class MyFileVisitor implements FileVisitor<Path> {
         Path newPath = dest.resolve(src.relativize(path));
         try {
             Files.copy(path, newPath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (Exception e) {
-            System.out.println("113");
-            e.printStackTrace();
+        } catch (IOException e) {
+             e.printStackTrace();
         }
         return FileVisitResult.CONTINUE;
     }
@@ -62,28 +60,13 @@ class MyFileVisitor implements FileVisitor<Path> {
         Path newPath = dest.resolve(src.relativize(path));
         try {
             Files.copy(path, newPath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (Exception e) {
-            System.out.println("112");
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return FileVisitResult.CONTINUE;
     }
-
-    @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exc) /*throws IOException */{
-        try {
-            System.out.println("Нет доступа к файлу");
-        } catch (Exception e) {
-            System.out.println("111");
-            e.printStackTrace();
-        }
-        return FileVisitResult.CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        return null;
-    }
-
 
 }
+
+
+
