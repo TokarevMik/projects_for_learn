ec631a1e517da423c9725a2b2248563996f6562f
stiks84@gmail.com <Rfhfv,jkm14>
2020-12-02 19:03:01 +0500

    10.2 'hibernate' upd1

--- 10_SQLAndHibernate/hibernateExp/src/main/java/Main.java
+++ 10_SQLAndHibernate/hibernateExp/src/main/java/Main.java
@@ -16,23 +16,26 @@ public class Main {
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
         Session session = sessionFactory.openSession();
         String idCourse;
-        do{
-        System.out.println("Введите id курса ");
-        idCourse = scn.nextLine();}
+        do {
+            System.out.println("Введите id курса ");
+            idCourse = scn.nextLine();
+        }
         while (!isNumber(idCourse));
         int idNum = Integer.parseInt(idCourse);
-        Course course = session.get(Course.class,idNum);
+        Course course = session.get(Course.class, idNum);
         System.out.println("Название курса - " + course.getName() + "Количество студентов на куре - " + course.getStudentsCount());
 
 
         sessionFactory.close();
-        }
-        public static boolean isNumber(String a){
-        try{int i = Integer.parseInt(a);}
-        catch (NumberFormatException | NullPointerException e){
+    }
+
+    public static boolean isNumber(String a) {
+        try {
+            int i = Integer.parseInt(a);
+        } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
-        return  true;
-        }
+        return true;
     }
+}
 
