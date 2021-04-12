fc51a7214ac67dd4cd9588ff3cfc16029a880991
stiks84@gmail.com <Rfhfv,jkm14>
2021-04-12 22:12:18 +0500

    13.21 "ForkJoinPool" 1.3

--- 11_Multithreading/ForkJoinPool/src/Node.java
+++ 11_Multithreading/ForkJoinPool/src/Node.java
@@ -2,19 +2,32 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Node {
     String url;
+    String domain = "https://skillbox.ru";
+    int levelOfPage = 0;
+
+    public int getLevelOfPage() {
+        return levelOfPage;
+    }
 
     public Node(String url) {
         this.url = url;
     }
 
-    private Collection<Node> nodes = new ArrayList<>();
+    public Node(String url, int levelOfPage) {
+        this.url = url;
+        this.levelOfPage = levelOfPage;
+    }
+
+    private Collection<Node> nodes = new HashSet<>();
 
     public String getUrl() {
         return url;
@@ -23,24 +36,27 @@ public class Node {
     public Collection<Node> getChildren() {
         try {
             Thread.sleep(200);
-            Document doc = Jsoup.connect(url).timeout(0).maxBodySize(0).get();
+            Document doc = Jsoup.connect(url).timeout(0).userAgent("Mozilla").maxBodySize(0).get();
             Element content = doc.body();
             Elements links = content.getElementsByTag("a");
             for (Element link : links) {
                 String linkHref = link.attr("href");
-                Pattern pattern = Pattern.compile("^/[\\w,-,_]+/$");
-                Pattern pattern2 = Pattern.compile("https://skillbox.ru/[\\w,\\D]+/$");
+                Pattern pattern = Pattern.compile("https://skillbox.ru/[\\w,\\D]+/$");
+                Pattern pattern2 = Pattern.compile("^/[\\w,-,_]+/$");
                 Matcher matcher = pattern.matcher(linkHref);
                 Matcher matcher2 = pattern2.matcher(linkHref);
-                if (matcher2.matches()) {
-                    nodes.add(new Node(linkHref));
-                }
                 if (matcher.matches()) {
-                    if (url.endsWith("/")) {
-                        linkHref = linkHref.substring(1);
+                        nodes.add(new Node(linkHref, takePageLevel(linkHref)));
+
+                }
+                if (matcher2.matches()) {
+                    linkHref = domain.concat(linkHref);
+                    try {
+                        nodes.add(new Node(linkHref, takePageLevel(linkHref)));
+                    } catch (IndexOutOfBoundsException e) {
+                        e.printStackTrace();
+                        System.out.println(linkHref + " ********");
                     }
-                    linkHref = url.concat(linkHref);
-                    nodes.add(new Node(linkHref));
                 }
             }
         } catch (
@@ -48,5 +64,33 @@ public class Node {
             e.printStackTrace();
         }
         return nodes;
+    }
+
+/*    private String getChildAdress(String parent, String child) throws IndexOutOfBoundsException {
+        int countCharParent = 0;
+        int positionnChild = 0;
+        for (int i = parent.length(); i < child.length(); i++) {
+            if (child.charAt(i) == '/') {
+                positionnChild = i;
+                break;
+            }
+        }
+        child = parent.concat(child.substring(parent.length(), positionnChild + 1));
+        return child;
+    }*/
+
+    private int takePageLevel(String adress) {
+        int count = 0;
+        for (int i = 0; i < adress.length(); i++) {
+            if (adress.charAt(i) == '/') count++;
+        }
+        int resulte = switch (count) {
+            case 4 -> 1;
+            case 5 -> 2;
+            case 6 -> 3;
+            case 7 -> 4;
+            default -> 0;
+        };
+        return resulte;
     }
 }
--- 11_Multithreading/ForkJoinPool/src/ParseNode.java
+++ 11_Multithreading/ForkJoinPool/src/ParseNode.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.RecursiveTask;
 
 public class ParseNode extends RecursiveTask<List<String>> {
@@ -8,22 +10,28 @@ public class ParseNode extends RecursiveTask<List<String>> {
     public ParseNode(Node note) {
         this.node = note;
     }
-    public static List <String> isAlreadyRead = new ArrayList<>();
+    public static Set<String> isAlreadyRead = new HashSet<>();
     public List<String> compute() {
         List<String> linksNames = new ArrayList<>();
-        linksNames.add(" ".concat(node.url));
+        String adress = node.getUrl();
+        if (node.levelOfPage>0){
+            for (int i = 0;i<= node.getLevelOfPage();i++){
+                adress = "   ".concat(adress);
+            }
+        }
+        linksNames.add(adress);
         List<ParseNode> taskList = new ArrayList<>();
         for (Node child : node.getChildren()) {
             if (!isAlreadyRead.contains(child.getUrl())){
                 isAlreadyRead.add(child.getUrl());
-            ParseNode task = new ParseNode(child);
-            task.fork();
-            try {
-                Thread.sleep(150);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            taskList.add(task);
+                ParseNode task = new ParseNode(child);
+                task.fork();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                taskList.add(task);
             }
         }
         for (ParseNode task : taskList) {
