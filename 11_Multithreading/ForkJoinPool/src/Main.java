e881f88c922abf97a23f482b18434c98f694c7f8
stiks84@gmail.com <Rfhfv,jkm14>
2021-04-17 16:03:52 +0500

    13.21 "ForkJoinPool" 1.6

--- 11_Multithreading/ForkJoinPool/src/Main.java
+++ 11_Multithreading/ForkJoinPool/src/Main.java
@@ -6,13 +6,13 @@ import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String url = "https://skillbox.ru";
         Node root = new Node(url);
-        File file = new File("\\file.txt");
+        File file = new File("file.txt");
         Set<String> sum = new ForkJoinPool().invoke(new ParseNode(root));
         sum.forEach(System.out::println);
-        try (FileWriter fl = new FileWriter(file, true);) {
+        try (FileWriter fl = new FileWriter(file, true)) {
             sum.forEach(s -> {
                 try {
                     fl.write(s + "\n");
