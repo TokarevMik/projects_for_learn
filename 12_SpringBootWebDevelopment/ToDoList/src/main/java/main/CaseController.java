6e852c03d3b65832b34c479c82f6ee6942b3d735
stiks84@gmail.com <3457857a>
2021-05-31 18:43:07 +0500

    14.9 'rest api' 1.1

--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/CaseController.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/CaseController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/cases")
 public class CaseController {
-    @GetMapping("/cases")
+    @GetMapping
     public List<Case> toDoList() {
         return CasesList.getToDoList();
     }
 
-    @GetMapping("/cases/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity getCaseFromList(@PathVariable int id) {
         Case currentCase = CasesList.getToDoList().get(id);
         if (currentCase == null) {
@@ -22,39 +23,39 @@ public class CaseController {
         return new ResponseEntity(currentCase, HttpStatus.OK);
     }
 
-    @PostMapping("/cases")
+    @PostMapping
     public int postNewCase(@RequestParam String name) {
         Case c = new Case();
         c.setName(name);
         return CasesList.addCase(c);
     }
 
-    @PostMapping("/cases/{id}")
+    @PostMapping("/{id}")
     public ResponseEntity postById(@PathVariable int id) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(null);
     }
 
-    @PutMapping("/cases/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity putOneCase(@PathVariable int id, @RequestParam String name) {
         if (CasesList.containCase(id)) {
             CasesList.toDoListUpdater(id, name);
             return ResponseEntity.status(HttpStatus.OK).body(null);
-        } else  return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
-    @PutMapping("/cases")
+    @PutMapping
     public List<Case> allCasesUpdater(@RequestParam List<Case> casesArrey) {
         CasesList.removeAllCases();
         casesArrey.stream().forEach(CasesList::addCase);
         return CasesList.getToDoList();
     }
 
-    @DeleteMapping("/cases")
+    @DeleteMapping
     public void deleteAllCases() {
         CasesList.removeAllCases();
     }
 
-    @DeleteMapping("/cases/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity deleteById(@PathVariable int id) {
         if (!CasesList.containCase(id)) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/CasesList.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/CasesList.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -6,33 +7,39 @@ import java.util.Map;
 
 public class CasesList {
     private static int currentId = 1;
-    static private Map<Integer,Case> toDoList = new HashMap<Integer,Case>();
-    static public synchronized void toDoListUpdater(int id, String name){
-            Case c= new Case();
-            c.setId(id);
-            c.setName(name);
-        toDoList.put(id,c);
+    static private Map<Integer, Case> toDoList = new HashMap<Integer, Case>();
+
+    static public synchronized void toDoListUpdater(int id, String name) {
+        Case c = new Case();
+        c.setId(id);
+        c.setName(name);
+        toDoList.put(id, c);
     }
-    static boolean containCase(int id){
-        if (toDoList.containsKey(id)){
+
+    static boolean containCase(int id) {
+        if (toDoList.containsKey(id)) {
             return true;
         } else return false;
     }
-    static public synchronized int addCase(Case c){
+
+    static public synchronized int addCase(Case c) {
         int id = currentId++;
         c.setId(id);
-        toDoList.put(id,c);
+        toDoList.put(id, c);
         return id;
     }
-    static public synchronized List<Case> getToDoList(){
+
+    static public synchronized List<Case> getToDoList() {
         List<Case> allCases = new ArrayList<>();
         allCases.addAll(toDoList.values());
         return allCases;
     }
-    static synchronized void  removeAllCases(){
+
+    static synchronized void removeAllCases() {
         toDoList.clear();
     }
-    static synchronized void removeCase(int id){
+
+    static synchronized void removeCase(int id) {
         toDoList.remove(id);
     }
 }
