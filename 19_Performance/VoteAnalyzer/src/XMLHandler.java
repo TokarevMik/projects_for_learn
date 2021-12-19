8145febbae47555db449659895451dd55790c684
stiks84@gmail.com <3457857a>
2021-12-19 15:40:33 +0500

    VoteAnalyzer DBConnection 1.3

--- 19_Performance/VoteAnalyzer/src/XMLHandler.java
+++ 19_Performance/VoteAnalyzer/src/XMLHandler.java
@@ -2,65 +2,50 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+import java.sql.SQLException;
 
 public class XMLHandler extends DefaultHandler {
-    private static SimpleDateFormat visitDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
-    private static SimpleDateFormat birthDayFormat = new SimpleDateFormat("yyyy.MM.dd");
-    WorkTime workTime;
-    HashMap<Integer, WorkTime> voteStationWorkTimes = new HashMap<>();
-    //    WorkTime[] voteStationWorkTimes = new WorkTime[180];
-    private static HashMap<Voter, Integer> voterCounts = new HashMap<>();
-    Voter voter;
-
+    private static StringBuilder insertQuery = new StringBuilder(7990);
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        try {
-            if (qName.equals("voter")) {
-                String name = attributes.getValue("name");
-                Date birthDate = birthDayFormat.parse(attributes.getValue("birthDay"));
-                voter = new Voter(name, birthDate);
-                Integer count = voterCounts.get(voter);
-                voterCounts.put(voter, count == null ? 1 : count + 1);
-            }
-            if (qName.equals("visit")) {
-                int station = Integer.parseInt(attributes.getValue("station"));
-                Date time = visitDateFormat.parse(attributes.getValue("time"));
-                workTime = voteStationWorkTimes.get(station);     //проверка HashMap
-//                workTime = voteStationWorkTimes[station];   //проверка цикла
-                if (workTime == null) {
-                    workTime = new WorkTime();
-//                    voteStationWorkTimes[station] = workTime; //проверка цикла
-                    voteStationWorkTimes.put(station, workTime);  //проверка HashMap
-                }
-                workTime.addVisitTime(time.getTime());
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
+        if (qName.equals("voter")) {
+            String name = attributes.getValue("name");
+            String birthDate = attributes.getValue("birthDay");
+            countVoter(name, birthDate);
         }
 
-    }//end of startElement
+    } //
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        if (qName.equals("voter")) voter = null;
-        if (qName.equals("visit")) workTime = null;
+        if (qName.equals("voter")) {
+            if (insertQuery.length() > 80000) {
+                try {
+                    DBConnection.executeMultiInsert(insertQuery);
+                    insertQuery = new StringBuilder(7990);
+                    System.out.println("Сброс");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (qName.equals("voters")) {
+            try {
+                DBConnection.executeMultiInsert(insertQuery);
+                System.out.println("Сброс финальный");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            insertQuery = new StringBuilder();
+        }
+
     }
 
-    public HashMap getWorkTime() {
-        return voteStationWorkTimes;
-    }  //
+    public static void countVoter(String name, String birthDay) {
+        birthDay = birthDay.replace('.', '-');
+        boolean isStart = insertQuery.length() == 0;
+        insertQuery.append((isStart ? "" : ",") + "('" + name + "', '" + birthDay + "', 1)");
 
-    /*public WorkTime[] getWorkTime() {
-        return voteStationWorkTimes;
-    }*/   //
-
-    public HashMap getVoterCounter() {
-        return voterCounts;
     }
-
 }
